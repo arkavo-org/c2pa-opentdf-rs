802f92ea352f7fa3a2b133b34f0df8b3c48d64a0
Paul Flynn <paul@arkavo.com>
2024-11-03 21:32:57 -0500

    Add test fixtures, dependencies, and enhanced tests
    Introduced certificates, keys, and a logo image for testing, added necessary dependencies to Cargo.toml. Enhanced `it_works` test to sign a file using C2PA and verify its contents.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # c2pa-opentdf-rs
 C2PA OpenTDF library in Rust community-driven
+
+## Development
+
+## Certificates & Keys
+
+https://github.com/contentauth/c2pa-rs/tree/main/sdk/tests/fixtures/certs
--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works_too() {
         let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+        assert_eq!(result, 4);}
+
+    #[test]
+    fn it_works() -> Result<(), Box<dyn std::error::Error>> {
+        use c2pa::{create_signer, Builder, SigningAlg};
+        use serde::Serialize;
+
+        #[derive(Serialize)]
+        struct Test {
+            my_tag: usize,
+        }
+        let signer = create_signer::from_keys(
+            &std::fs::read("tests/data/cert.pem")?,      // read certificate
+            &std::fs::read("tests/data/private.pem")?,   // read private key
+            SigningAlg::Es256,                // ECDSA with SHA-256
+            None,                             // no timestamp authority
+        )?;
+        let mut builder = Builder::from_json(r#"{"title": "Test"}"#)?;
+        builder.add_assertion("org.arkavo.test", &Test { my_tag: 42 })?;
+        std::fs::remove_file("target/tmp/logo_sign.png")?;
+        // embed a manifest using the signer
+        builder.sign_file(
+            &*signer,
+            "tests/data/logo.png",
+            "target/tmp/logo_sign.png",
+        )?;        Ok(())
+        }
 }
